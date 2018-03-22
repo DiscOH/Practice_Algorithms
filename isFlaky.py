adf15baa7fd4a7eaa676f5c70a721892dd46ae1b
BNelson3 <bnelson3@apple.com>
2018-03-22 14:00:24 -0700

    updated with formating

--- isFlaky.py
+++ isFlaky.py
@@ -2,11 +2,11 @@ from random import randint
 
 
 def create_history(size=100):
-    return [randint(0,5) > 0 for x in range(size)]
+    return [randint(0, 5) > 0 for _ in range(size)]
 
 
 def is_flaky(value):
-    return value < 1
+    return "is{} flaky.\nValue is {}\n".format(" not" * (value > 1), value)
 
 
 def square(history=False):
@@ -23,9 +23,9 @@ def square(history=False):
             power = 1
         previous = h
         if h:
-            success += 2 ** power
+            success += 4 ** power
         else:
-            failure += 2 ** power
+            failure += 4 ** power
     return float(success) / failure
 
 
@@ -40,7 +40,7 @@ def diminishing(history=False):
             success += h * coefficient
         else:
             failure += h * coefficient
-        coefficient *= .75
+        coefficient *= .5
     return success / failure
 
 
@@ -59,15 +59,15 @@ def diminishing_square(history=False):
             power = 1
         previous = h
         if h:
-            success += 2 ** (power * coefficient)
+            success += 4 ** (power * coefficient)
         else:
-            failure += 2 ** (power * coefficient)
-        coefficient *= .75
-    return success / failure
+            failure += 4 ** (power * coefficient)
+        coefficient *= .5
+    return float(success) / failure
 
 
 if __name__ == '__main__':
-    history = [1, 1, 1, 1, 0, 1, 0, 1, 0, 1, 0, 1, 1, 0, 1, 1, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0]
-    print("square algorithm: {}".format(square(history)))
-    print("diminishing algorithm: {}".format(diminishing(history)))
-    print("combined algorithm: {}".format(diminishing_square(history)))
+    history = [1, 1, 1, 1, 0, 1, 0, 1, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0]
+    print("square algorithm: {}".format(is_flaky(square(history))))
+    print("diminishing algorithm: {}".format(is_flaky(diminishing(history))))
+    print("combined algorithm: {}".format(is_flaky(diminishing_square(history))))
